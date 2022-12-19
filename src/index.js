ff0230adfa4eca524e1facd306d1bbf191e26332
Naeem <naeemark@gmail.com>
2022-12-19 02:36:31 +0100

    Complete Get flow

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ require('module-alias/register');
 const hello = require('./handler/hello');
 
 const createTodoV1 = require('./handler/v1/todo/create');
+const getTodoV1 = require('./handler/v1/todo/get');
 
 module.exports = {
   hello,
-  createTodoV1
+  createTodoV1,
+  getTodoV1
 };
--- src/services/todo/todo.service.js
+++ src/services/todo/todo.service.js
@@ -3,11 +3,19 @@
  *
  */
 const { v4: uuidv4 } = require('uuid');
+const { APIError } = require('@utils/APIError');
 
 const repository = require('../../repositories/todo');
 
 const create = async requestBody => repository.createItem({ id: uuidv4(), ...requestBody });
 
+const get = async (id) => {
+  const item = await repository.getItem(id);
+  if (!item) throw APIError.notFound();
+  return item;
+};
+
 module.exports = {
-  create
+  create,
+  get
 };
